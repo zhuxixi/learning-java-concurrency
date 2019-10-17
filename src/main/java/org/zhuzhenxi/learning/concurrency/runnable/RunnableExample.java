76cf1d4db400fe7b672fff7b12bf3e77dc18a4f6
zhuzhenxi <zhuzhenxi_555@hotmail.com>
2019-10-17 09:51:40 +0800

    新增Runnable 测试

--- src/main/java/org/zhuzhenxi/learning/concurrency/runnable/RunnableExample.java
+++ src/main/java/org/zhuzhenxi/learning/concurrency/runnable/RunnableExample.java
@@ -1,6 +1,5 @@
 package org.zhuzhenxi.learning.concurrency.runnable;
 
-import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @author zhuzh
@@ -10,6 +9,12 @@ public class RunnableExample {
 
     public static void main(String[] args){
         Runnable runner = () -> System.out.println(Thread.currentThread().getName());
+        //直接调用run方法，不会启动新的线程
         runner.run();
+        Thread thread = new Thread(runner);
+        //直接调用thread.run也不会启动新的线程
+        thread.run();
+        //只有调用start才会启动新的线程
+        thread.start();
     }
 }
