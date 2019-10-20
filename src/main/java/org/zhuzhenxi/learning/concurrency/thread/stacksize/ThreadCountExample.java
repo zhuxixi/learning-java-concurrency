4616273333968fa1c285ac0fc98478e8a7ae29d1
zhuzhenxi <zhuzhenxi_555@hotmail.com>
2019-10-20 14:57:27 +0800

    测试

---
+++ src/main/java/org/zhuzhenxi/learning/concurrency/thread/stacksize/ThreadCountExample.java
@@ -0,0 +1,17 @@
+package org.zhuzhenxi.learning.concurrency.thread.stacksize;
+
+/**
+ * 指定Stack size改变线程数量
+ * @author zhuzh
+ * @date 2019.10.20
+ */
+public class ThreadCountExample {
+    public static void main(String[] args){
+        int i = 0;
+        while (true){
+            System.out.println(i);
+            Thread thread = new StackSizeExampleThread(null,new WaitTask(),i+"",20);
+            thread.start();
+        }
+    }
+}
